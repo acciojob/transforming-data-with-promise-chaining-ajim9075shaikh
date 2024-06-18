7ce8d1fe26593c5805c2c79b9705c7c7515090e9
ajim9075shaikh <142928027+ajim9075shaikh@users.noreply.github.com>
2024-06-18 12:02:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const btn=document.getElementById("btn").addEventListener("click", clickFunction)
+const input=document.getElementById("ip").value
+let finalResult;
+let promise=new Promise(function(resolve, reject) {
+  setTimeout(function() {
+    resolve(1);
+  }, 2000);
+})
+promise
+   .then(function(result) {
+    return result * 2;
+  })
+  .then(function(result) {
+    return result - 3;
+  })
+  .then(function(result) {
+    return result / 2;
+  })
+  .then(function(result) {
+    finalResult = result + 10;
+    console.log(finalResult); // Print final result
+  });
+.catch(function(error) {
+    console.log('Error occurred!', error);
+  });
